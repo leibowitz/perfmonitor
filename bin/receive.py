176c972aef3fbc2c844355b2c62040a69d9b9f91
Gianni Moschini <gianni.moschini+git@gmail.com>
2013-02-25 20:00:50 +0000

    Remove useless comments in receive.py

--- bin/receive.py
+++ bin/receive.py
@@ -5,6 +5,7 @@ import json
 import os
 from pymongo import MongoClient
 
+
 dbcon = MongoClient()
 
 NETSNIFF_UTIL = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'tools', 'netsniff.js')
@@ -28,15 +29,8 @@ def callback(ch, method, properties, body):
     print ' [x] Executing command phantomjs', content['url']
     harcontent = subprocess.check_output(['phantomjs', NETSNIFF_UTIL, content['url']])
     jscontent = json.loads(harcontent)
-    print jscontent
     dbcon.perfmonitor.har.insert(jscontent)
 
-    #path = os.path.join('harfiles', content['account'], content['site'])
-    #if not os.path.exists(path):
-    #    os.makedirs(path)
-    #f = open(os.path.join(path, 'file.har'), 'w')
-    #f.write(harcontent)
-    #f.close()
     ch.basic_ack(delivery_tag = method.delivery_tag)
     print " [x] Done"
 
