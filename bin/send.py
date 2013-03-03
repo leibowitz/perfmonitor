f12500c836d2d5f04d3ad68b2b227f11b68af136
Gianni Moschini <gianni.moschini+git@gmail.com>
2013-03-03 22:57:13 +0000

    Add python script to send example task to the queue to test it

---
+++ bin/send.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+import pika
+import json
+
+msg = {
+    'url':'http://www.google.co.uk',
+    'site': 'gtk',
+    'account': 'me',
+    'type': 'har'
+}
+connection = pika.BlockingConnection(pika.ConnectionParameters(
+               'localhost'))
+channel = connection.channel()
+channel.exchange_declare(exchange='perfmonitor', type='direct')
+channel.queue_declare(queue='perf')
+channel.basic_publish(exchange='perfmonitor',
+                      routing_key='perftest',
+                      body=json.dumps(msg))
+print " [x] Sent 'Hello World!'"
+connection.close()
