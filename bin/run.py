8feacc98db0f3e49e6589ef309212e15bd9fae2a
Gianni Moschini <gianni.moschini+git@gmail.com>
2013-04-22 21:26:11 +0100

    Use browsermob python module to set capture_headers to true to browsermob proxy

--- bin/run.py
+++ bin/run.py
@@ -1,27 +1,35 @@
 from browsermobproxy import Server
+from browsermob import BrowserMobProxy, BrowserMobProxyHub
 from selenium import webdriver
 import json
 import sys
 import os
 import time 
+from urlparse import urlparse
 from distutils.spawn import find_executable
 
 server = Server(find_executable('browsermob-proxy'))
 server.start()
-# server._is_listening()
-proxy = server.create_proxy()
+url = urlparse(server.url)
+hub = BrowserMobProxyHub(hostname = url.hostname, port = url.port)
+proxy = hub.get_proxy(capture_headers=True, capture_content=False)
+
+proxy.new_har("google")
+
+proxy_url = "http://%s:%s" %(proxy.hub.hostname, proxy.port)
+se_proxy = webdriver.Proxy({"httpProxy": proxy_url})
+
 driver = None
 
 browser = 'Firefox'
 if browser == 'Firefox':
     profile = webdriver.FirefoxProfile()
-    profile.set_proxy(proxy.selenium_proxy())
+    profile.set_proxy(se_proxy)
     driver = webdriver.Firefox(firefox_profile=profile)
 
-if driver is not None:
-    proxy.new_har("google")
+if driver:
     driver.get("http://www.google.co.uk")
-    har = proxy.har() 
+    har = proxy.get_har()
 
     # get onload time from web timing api, if available
     timing = driver.execute_script("""
@@ -32,8 +40,9 @@ if driver is not None:
         har['log']['pages'][0]['pageTimings']['onLoad'] = timing
 
     print json.dumps(har, indent=4)
+
     driver.quit()
 
-proxy.close()
+proxy.close_proxy()
 server.stop()
 sys.exit(0)
