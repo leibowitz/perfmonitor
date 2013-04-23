4a94a2467619a9a750b554589c957f2058adbf33
Gianni Moschini <gianni.moschini+git@gmail.com>
2013-04-23 01:09:45 +0100

    Add support for PhantomJS Use ghostdriver mode and proxy setting

--- bin/run.py
+++ bin/run.py
@@ -6,38 +6,52 @@ import sys
 import os
 import time 
 from urlparse import urlparse
-from distutils.spawn import find_executable
 
-server = Server(find_executable('browsermob-proxy'))
+if len(sys.argv) < 2:
+    sys.exit(1)
+
+url = sys.argv[1]
+
+server = Server('/home/gianni/browsermob-proxy/browsermob-proxy-2.0-beta-7/bin/browsermob-proxy')
+
 server.start()
-url = urlparse(server.url)
-hub = BrowserMobProxyHub(hostname = url.hostname, port = url.port)
+server_url = urlparse(server.url)
+hub = BrowserMobProxyHub(hostname = server_url.hostname, port = server_url.port)
 proxy = hub.get_proxy(capture_headers=True, capture_content=False)
 
-proxy.new_har("google")
+proxy.new_har()
 
-proxy_url = "http://%s:%s" %(proxy.hub.hostname, proxy.port)
-se_proxy = webdriver.Proxy({"httpProxy": proxy_url})
+proxy_url = "%s:%s" %(proxy.hub.hostname, proxy.port)
+proxy_settings = {
+    'httpProxy': proxy_url,
+    'ftpProxy': proxy_url,
+    'sslProxy': proxy_url,
+}
+
+se_proxy = webdriver.Proxy(proxy_settings)
 
 driver = None
 
 browser = 'Firefox'
+timingapi = True
+
 if browser == 'Firefox':
-    profile = webdriver.FirefoxProfile()
-    profile.set_proxy(se_proxy)
-    driver = webdriver.Firefox(firefox_profile=profile)
+    driver = webdriver.Firefox(proxy=se_proxy)
+elif browser == 'PhantomJS':
+    driver = webdriver.PhantomJS(proxy=proxy_url) 
 
 if driver:
-    driver.get("http://www.google.co.uk")
+    driver.get(url)
     har = proxy.get_har()
 
     # get onload time from web timing api, if available
-    timing = driver.execute_script("""
-    var t = window.performance.timing;
-    return t.loadEventStart-t.navigationStart;
-    """)
-    if type(timing) == type(1) and timing: 
-        har['log']['pages'][0]['pageTimings']['onLoad'] = timing
+    if timingapi:
+        timing = driver.execute_script("""
+        var t = window.performance.timing;
+        return t.loadEventStart-t.navigationStart;
+        """)
+        if type(timing) == type(1) and timing: 
+            har['log']['pages'][0]['pageTimings']['onLoad'] = timing
 
     print json.dumps(har, indent=4)
 
