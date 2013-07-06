15fe80176dcfc74c668c07af80eb229ec38ed33a
Gianni Moschini <gianni.moschini+git@gmail.com>
2013-07-06 10:50:11 +0100

    Move cron task to the tasks file, update celeryconfig

--- bin/celeryconfig.py
+++ bin/celeryconfig.py
@@ -10,49 +10,49 @@ from datetime import timedelta
 CELERYBEAT_SCHEDULE = {
     # 252 times a day
     'every-5-minutes': {
-        'task': 'cron.process',
+        'task': 'tasks.processcron',
         'schedule': timedelta(minutes=5),
         'args': ([5]),
     },
     # 126 times a day
     'every-10-minutes': {
-        'task': 'cron.process',
+        'task': 'tasks.processcron',
         'schedule': timedelta(minutes=10),
         'args': ([10]),
     },
     # 42 times a day
     'every-30-minutes': {
-        'task': 'cron.process',
+        'task': 'tasks.processcron',
         'schedule': timedelta(minutes=30),
         'args': ([30]),
     },
     # 24 times a day
     'every-60-minutes': {
-        'task': 'cron.process',
+        'task': 'tasks.processcron',
         'schedule': timedelta(minutes=60),
         'args': ([60]),
     },
     # 8 times a day
     'every-180-minutes': {
-        'task': 'cron.process',
+        'task': 'tasks.processcron',
         'schedule': timedelta(minutes=180),
         'args': ([180]),
     },
     # 4 times a day
     'every-360-minutes': {
-        'task': 'cron.process',
+        'task': 'tasks.processcron',
         'schedule': timedelta(minutes=360),
         'args': ([360]),
     },
     # twice a day
     'every-720-minutes': {
-        'task': 'cron.process',
+        'task': 'tasks.processcron',
         'schedule': timedelta(minutes=720),
         'args': ([720]),
     },
     # once a day
     'every-1440-minutes': {
-        'task': 'cron.process',
+        'task': 'tasks.processcron',
         'schedule': timedelta(minutes=1440),
         'args': ([1440]),
     },
--- bin/tasks.py
+++ bin/tasks.py
@@ -40,3 +40,32 @@ def processtest(content):
         print ' [x] Unable to save HAR response, sending back'
         return False
 
+@celery.task
+def processcron(minutes):
+    print 'Running cron of tasks for every %d minutes' % (minutes)
+    dbcon = MongoClient()
+    rows = dbcon.perfmonitor.sites.aggregate([
+        {
+         '$match': {'interval': minutes}
+        }, 
+        {'$unwind': "$urls"} 
+    ])
+
+    if not rows['result']:
+        print 'No tasks found to run every %d minutes' % (minutes)
+        return False
+
+    for row in rows['result']:
+        msg = {
+            'url': str(row['urls']),
+            'site': str(row['site']),
+            'account': 'me',
+            'type': 'har',
+            'nb': int(row['nb']),
+            'agent': str(row['agent'])
+        }
+
+        processtest.delay(msg)
+    
+    print 'Done running tasks for every %d minutes' % (minutes)
+    return True
